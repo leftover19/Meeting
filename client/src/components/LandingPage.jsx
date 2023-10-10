3833683f1918f84b6681eb8c8f4a5eeca1343265
leftover19 <vishwaskapoor27@gmail.com>
2023-10-10 20:22:51 +0530

    fixed

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,6 +1,17 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export function LandingPage() {
+    const navigate = useNavigate();
+    const [count, setCount] = useState(1);
     return <div>
         Hi welcome to landing page
-    </div>
+        <Button variant="contained" onClick={() => {
+            setCount(count + 1);
+            navigate(`/meeting/${count}`);
+        }
+        }> Go to Meeting {count} </Button>
+    </div >
 }
